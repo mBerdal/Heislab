de0011adbd8e88a3913cf7d59c24e684e3da43c6
Magnus Berdal <magberd@stud.ntnu.no>
2018-02-23 14:12:15 +0100

    mod 8

--- main.c
+++ main.c
@@ -154,7 +154,6 @@ void get_job(int matrix[N_FLOORS][3], int* current_dir, int* current_floor){
     go_to_order(matrix, current_dir, current_floor); //Check for orders for elevator to get to floor
     if(*current_dir == 0){
       *current_dir = set_destination(matrix, *current_floor); //Check for orders for elevator to go to floor
-      printf("%d", *current_dir);
     }
   }
 }
