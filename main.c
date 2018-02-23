6c036659a22f578b236a40f4688c421d03d945fa
Magnus Berdal <magberd@stud.ntnu.no>
2018-02-23 13:50:55 +0100

    mod 8

--- main.c
+++ main.c
@@ -4,6 +4,22 @@
 #include "math.h"
 #include <stdlib.h>
 
+
+#include <time.h>
+#include <stdio.h>
+
+// **** Contains functions for the timer ****
+
+time_t start_value;
+time_t end_value;
+
+// Starts the timer
+void start_timer();
+
+// Checks if time seconds have past
+// @return 1 if time seconds have past, return 0 otherwise
+int check_timer(double seconds);
+
 typedef int bool;
 #define true 1
 #define false 0
